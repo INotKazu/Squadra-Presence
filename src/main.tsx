6a4f212be59e81936f76c2c5de49f47f0f87021b
Kazuma <86324148+INotKazu@users.noreply.github.com>
2026-08-31 21:02:48 -0500

    feat: activate isolated SMITE 2 companion shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import Smite2App from "./smite2/Smite2App";
+import "./smite2/smite2.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Smite2App />
   </StrictMode>,
 );
